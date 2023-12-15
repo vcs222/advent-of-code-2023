a1c9e9027503cf111c79bb6977018c76f7bc1bcc
Csaba Veres <vcs222@gmail.com>
2023-12-15 10:14:08 +0100

    fix tests

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -69,7 +69,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(142));
+        assert_eq!(result, Some(219));
     }
 
     #[test]
